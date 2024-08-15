4d341c7e5e5c815632e841307799fe96f644311f
deepak sodhi <deepaksodhi0023@gmail.com>
2024-08-16 01:03:03 +0530

    collection

--- src/models/product.models.js
+++ src/models/product.models.js
@@ -88,6 +88,6 @@ const productschema= new Schema(
 
 
     productschema.plugin(mongooseAggregatePaginate)
-
+ 
 // export const product = mongoose.model("final_product",productschema)
 export const product = mongoose.model("producttt",productschema)
